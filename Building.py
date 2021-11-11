9016286363351db26a3e37dcf9159040ed653e63
barakon11 <barakon111@gmail.com>
2021-11-11 18:47:14 +0200

    change names for python errors + updated data files

--- Building.py
+++ Building.py
@@ -1,13 +1,13 @@
 
 
 class Building:
-    def __init__(self, minFloor, maxFloor):
-        self.minFloor = minFloor
-        self.maxFloor = maxFloor
-        self.list_elvators = []
+    def __init__(self, min_floor, max_floor):
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.list_elevators = []
 
     def __str__(self) -> str:
-        for i in self.list_elvators:
+        for i in self.list_elevators:
             print(i)
 
         return ""
--- Elevator.py
+++ Elevator.py
@@ -1,23 +1,21 @@
 from CallForElevator import CallForElevator
 
+
 class Elevator:
-    def __init__(self, id: float, speed: float, minFloor: float, maxFloor: float, closeTime: float, openTime: float, startTime: float,
-                 stopTime: float) -> None:
+    def __init__(self, id: float, speed: float, min_floor: float, max_floor: float, close_time: float, open_time: float,
+                 start_time: float, stop_time: float) -> None:
         self.id = id
         self.speed = float(speed)
-        self.minFloor = float(minFloor)
-        self.maxFloor = float(maxFloor)
-        self.closeTime = float(closeTime)
-        self.openTime = float(openTime)
-        self.startTime = float(startTime)
-        self.stopTime = float(stopTime)
+        self.min_floor = float(min_floor)
+        self.max_floor = float(max_floor)
+        self.close_time = float(close_time)
+        self.open_time = float(open_time)
+        self.start_time = float(start_time)
+        self.stop_time = float(stop_time)
         self.calls = []
 
-
     def __str__(self) -> str:
-        return f"id:{self.id} speed:{self.speed} minFloor:{self.minFloor} maxFloor:{self.maxFloor} closeTime:{self.closeTime} openTime:{self.openTime} StartTime:{self.startTime} stopTime:{self.stopTime}"
+        return f"id:{self.id} speed:{self.speed} minFloor:{self.min_floor} maxFloor:{self.max_floor} closeTime:{self.close_time} openTime:{self.open_time} StartTime:{self.start_time} stopTime:{self.stop_time}"
 
-    def isEmpty(self):
-        return (not self.call)
-
-
+    def is_empty(self):
+        return not self.call
